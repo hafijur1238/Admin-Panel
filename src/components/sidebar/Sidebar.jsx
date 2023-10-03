5d14a628b9d0cee5fad2d936ae940e26e15bb03d
hafijur1238 <saadmalik12349@gmail.com>
2023-10-03 17:13:02 +0600

    Added new page & Changes some code

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -11,77 +11,86 @@ import AddToQueueIcon from '@mui/icons-material/AddToQueue';
 import SettingsIcon from '@mui/icons-material/Settings';
 import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import { Link } from "react-router-dom"
+
+// import {PortraitOutlinedIcon, Inventory2OutlinedIcon, BorderOuterOutlinedIcon, LocalShippingOutlinedIcon, InsertChartOutlinedIcon, MarkUnreadChatAltOutlinedIcon, SettingsSystemDaydreamOutlinedIcon, AddToQueueIcon, SettingsIcon, InsertEmoticonIcon, ExitToAppIcon} from "@mui/icons-material"
 
 const Sidebar = () => {
-  return (
-    <div className='sidebar'>
-        <div className="top">
-            <span className="logo">CloudeWindow</span>
+    return (
+        <div className='sidebar'>
+            <div className="top">
+                <Link to="/" style={{ textDecoration: "none" }}>
+                    <span className="logo">CloudeWindow</span>
+                </Link>
+            </div>
+            <hr />
+            <div className="center">
+                <ul>
+                    <p className="title">MAIN</p>
+                    <li>
+                        <GridViewIcon className="icon" />
+                        <span>Dashboard</span>
+                    </li>
+                    <p className="title">LISTS</p>
+                    <Link to="/users" style={{ textDecoration: "none" }}>
+                        <li>
+                            <PortraitOutlinedIcon className="icon" />
+                            <span>Users</span>
+                        </li>
+                    </Link>
+                    <Link to="/products" style={{ textDecoration: "none" }}>
+                        <li>
+                            <Inventory2OutlinedIcon className="icon" />
+                            <span>Products</span>
+                        </li>
+                    </Link>
+                    <li>
+                        <BorderOuterOutlinedIcon className="icon" />
+                        <span>Orders</span>
+                    </li>
+                    <li>
+                        <LocalShippingOutlinedIcon className="icon" />
+                        <span>Delivery</span>
+                    </li>
+                    <p className="title">USEFUL</p>
+                    <li>
+                        <InsertChartOutlinedIcon className="icon" />
+                        <span>Stats</span>
+                    </li>
+                    <li>
+                        <MarkUnreadChatAltOutlinedIcon className="icon" />
+                        <span>Notifications</span>
+                    </li>
+                    <p className="title">SERVICE</p>
+                    <li>
+                        <SettingsSystemDaydreamOutlinedIcon className="icon" />
+                        <span>System Health</span>
+                    </li>
+                    <li>
+                        <AddToQueueIcon className="icon" />
+                        <span>Logs</span>
+                    </li>
+                    <li>
+                        <SettingsIcon className="icon" />
+                        <span>Settings</span>
+                    </li>
+                    <p className="title">USER</p>
+                    <li>
+                        <InsertEmoticonIcon className="icon" />
+                        <span>Profile</span>
+                    </li>
+                    <li>
+                        <ExitToAppIcon className="icon" />
+                        <span>Logout</span>
+                    </li>
+                </ul>
+            </div>
+            <div className="bottom">
+                <div className="colorOption"></div>
+                <div className="colorOption"></div>
+            </div>
         </div>
-        <hr />
-        <div className="center">
-            <ul>
-                <p className="title">MAIN</p>
-                <li>
-                    <GridViewIcon className="icon"/>
-                    <span>Dashboard</span>
-                </li>
-                <p className="title">LISTS</p>
-                <li>
-                    <PortraitOutlinedIcon className="icon"/>
-                    <span>Users</span>
-                </li>
-                <li>
-                    <Inventory2OutlinedIcon className="icon"/>
-                    <span>Products</span>
-                </li>
-                <li>
-                    <BorderOuterOutlinedIcon className="icon"/>
-                    <span>Orders</span>
-                </li>
-                <li>
-                    <LocalShippingOutlinedIcon className="icon"/>
-                    <span>Delivery</span>
-                </li>
-                <p className="title">USEFUL</p>
-                <li>
-                    <InsertChartOutlinedIcon className="icon"/>
-                    <span>Stats</span>
-                </li>
-                <li>
-                    <MarkUnreadChatAltOutlinedIcon className="icon"/>
-                    <span>Notifications</span>
-                </li>
-                <p className="title">SERVICE</p>
-                <li>
-                    <SettingsSystemDaydreamOutlinedIcon className="icon"/>
-                    <span>System Health</span>
-                </li>
-                <li>
-                    <AddToQueueIcon className="icon"/>
-                    <span>Logs</span>
-                </li>
-                <li>
-                    <SettingsIcon className="icon"/>
-                    <span>Settings</span>
-                </li>
-                <p className="title">USER</p>
-                <li>
-                    <InsertEmoticonIcon className="icon"/>
-                    <span>Profile</span>
-                </li>
-                <li>
-                    <ExitToAppIcon className="icon"/>
-                    <span>Logout</span>
-                </li>
-            </ul>
-        </div>
-        <div className="bottom">
-            <div className="colorOption"></div>
-            <div className="colorOption"></div>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Sidebar
